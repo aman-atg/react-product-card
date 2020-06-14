3c63de26454a46e5c4bfe76fc948976e8ff9d250
aman-atg <amanansari.a2@gmail.com>
2020-06-14 21:59:47 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="Home">Hello World</div>;
+  return (
+    <div className="Home">
+      <div className="container">
+        <div className="card">
+          <div className="shoeBackground">
+            <div className="gradients">
+              <div className="gradient" class="blue"></div>
+              <div className="gradient" class="red"></div>
+              <div className="gradient" class="green"></div>
+              <div className="gradient" class="orange"></div>
+              <div className="gradient" class="black"></div>
+            </div>
+            <h1 className="nike">nike</h1>
+            <img src="./assets/img/logo.png" alt="logo" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
